b099e0883d3dc3c76181e8687e2f3a07bbe0b1b6
fluqui <jotayepes@icloud.com>
2015-08-11 11:04:05 -0700

    Añadiendo tematica

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,7 +12,7 @@ exports.load = function (req, res, next, quizId) {
 };
 
 // GET /quizes
-exports.index = function(req, res, next){
+exports.index = function(req, res){
 	if(req.query.search){
 		var search = '%' + req.query.search.replace(/\s/g,"%") + '%';
 
@@ -20,11 +20,11 @@ exports.index = function(req, res, next){
 			{where: ["pregunta like ?", search],
 			order:[['pregunta','ASC']]}
 			).then(function(quizes){
-				res.render('quizes/index', {quizes: quizes, errors: []});
+				res.render('quizes/index.ejs', {quizes: quizes, errors: []});
 			}).catch(function(error){next (error);});
 	}else{
 		models.Quiz.findAll().then(function (quizes) {
-			res.render('quizes/index', { quizes: quizes});
+			res.render('quizes/index.ejs', { quizes: quizes, errors: []});
 		}).catch(function(error) {next(error);});
 	}
 };
@@ -32,7 +32,7 @@ exports.index = function(req, res, next){
 
 // GET /quizes/:id
 exports.show = function(req, res){
-	res.render('quizes/show', { quiz: req.quiz});
+	res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -41,11 +41,75 @@ exports.answer = function(req, res){
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado}); 
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []}); 
+};
+
+// GET /quizes/new
+exports.new = function(req, res){
+	var quiz = models.Quiz.build(  // crea objeto quiz
+		{pregunta: "Pregunta", 
+		respuesta: "Respuesta",
+		tematica: "Tematica"});
+
+	res.render('quizes/new', {quiz: quiz, errors: []});
+}
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			}else{
+				quiz // save: guarda en DB los campos pregunta y respuesta de quiz
+				.save({fields: ["pregunta", "respuesta", "tematica"]})
+				.then(function(){res.redirect('/quizes')}) 
+			}		// Redirecciona HTTP (URL relativo) lista de preguntas
+		}
+	);
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // autoload de instancia de quiz
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+//PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tematica = req.body.quiz.tematica;
+
+	req.quiz
+	.validate()
+	.then(
+		function(error) {
+			if (err) {
+				res.render('quizes/edit', {quiz: req.quiz, errors: []});
+			} else{
+				req.quiz 	// save: guarda campos en DB
+				.save ({fields: ["pregunta", "respuesta", "tematica"]})
+				.then (function(){res.redirect('/quizes');});
+			}
+		}
+	);
+};
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res){
+	req.quiz.destroy().then(function() {
+		res.redirect('/quizes');
+	}).catch(function(error) {next(error)});
 };
 
 // GET /author
 
 exports.author = function (req, res){
-	res.render('author');
+	res.render('author', {errors: []});
 }
--- models/models.js
+++ models/models.js
@@ -16,8 +16,8 @@ sequelize.sync().success(function() {
 	// success(...) ejecuta el manejador una vez creada la tabla
 	Quiz.count().success(function (count){
 		if (count === 0) {
-			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma'});
-			Quiz.create({ pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}).then(function(){console.log('Base de datos inicializada')});
+			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma', tematica: 'Otro'});
+			Quiz.create({ pregunta: 'Capital de Portugal', respuesta: 'Lisboa', tematica: 'Otro'}).then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
